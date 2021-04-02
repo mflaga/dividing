ad6374d9ca4535628f2607f7f993cbf5f1f11271
Marek Flaga <m.flaga@gmail.com>
2021-04-02 12:36:21 +0200

    Dodano multiple i divide oraz wywołania funkcji

--- main.py
+++ main.py
@@ -6,6 +6,27 @@ def substract(a,b):
 	c = a-b
 	return c
 
+def multiple(a,b):
+	c = a*b
+	return c
+
+def divide(a,b):
+	try:
+		c = a/b
+		return c
+	except:
+		ZeroDivisionError
+		print('Dzielenie przez zero nie możliwe')
+		return ''
+
+
+
+
+
+
 
 print(adding(6,3))
 print(substract(6,3))
+print(multiple(6,3))
+print(divide(6,3))
+print(divide(6,0))
